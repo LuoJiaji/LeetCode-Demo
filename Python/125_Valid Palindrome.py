8fb9800bc083c37325771b86c582d864000600d1
Luojiaji <lt920@126.com>
2019-08-16 13:56:48 +0800

    add No.125

---
+++ Python/125_Valid Palindrome.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        # print(s)
+        # print(s.lower())
+        # print(s[0].isalnum())
+
+        i = 0
+        j = len(s) - 1
+
+        while i < j:
+            while i < j and not s[i].isalnum():
+                i += 1
+            while i < j and not s[j].isalnum():
+                j -= 1
+            if s[i].lower() != s[j].lower():
+                return False
+            i += 1
+            j -= 1
+        return True
+        
+
+data = "A man, a plan, a canal: Panama"
+
+result = Solution().isPalindrome(data)
+print(result)
