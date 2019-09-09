8aba526bd1800b926ce9a7fb95d753197b6b4f46
Luojiaji <lt920@126.com>
2019-09-09 22:05:43 +0800

    add weekly contest 153-b

--- Contest/weekly-contest-153/B.py
+++ Contest/weekly-contest-153/B.py
@@ -6,8 +6,31 @@ class Solution(object):
         :type year: int
         :rtype: str
         """
+        w = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+        c = int(year/100)
+        year = year % 100
+        # print(c)
+        if month == 1 or month == 2:
+            month += 12
+            year -= 1
+        week =  (year + int(year/4) + int(c/4) - 2 * c + int(26 * (month + 1)/10) + day - 1) % 7
+        # print(w[week])
+        return w[week]
         
-
 day = 31
 month = 8
 year = 2019
+result = Solution().dayOfTheWeek(day, month, year)
+print(result)
+
+day = 18 
+month = 7 
+year = 1999
+result = Solution().dayOfTheWeek(day, month, year)
+print(result)
+
+day = 15
+month = 8
+year = 1993
+result = Solution().dayOfTheWeek(day, month, year)
+print(result)
