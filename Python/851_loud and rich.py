140f0064387282c71c5e5a2db4e07ed95cbd8df5
Luojiaji <lt920@126.com>
2019-11-07 08:28:11 +0800

    fix 851 bug

--- Python/851_loud and rich.py
+++ Python/851_loud and rich.py
@@ -1,41 +1,39 @@
-import copy 
-class Solution(object):
-    def loudAndRich(self, richer, quiet):
-        """
-        :type richer: List[List[int]]
-        :type quiet: List[int]
-        :rtype: List[int]
-        """
-        bigger = {}
-        for i in richer:
-            if i[1] not in bigger:
-                bigger[i[1]] = []
-                bigger[i[1]].append(i[0])
-            else:
-                bigger[i[1]].append(i[0])
-        # print(bigger)
+# import copy 
+# class Solution(object):
+#     def loudAndRich(self, richer, quiet):
+#         """
+#         :type richer: List[List[int]]
+#         :type quiet: List[int]
+#         :rtype: List[int]
+#         """
+#         bigger = {}
+#         for i in richer:
+#             if i[1] not in bigger:
+#                 bigger[i[1]] = []
+#                 bigger[i[1]].append(i[0])
+#             else:
+#                 bigger[i[1]].append(i[0])
+#         # print(bigger)
 
-        l = len(quiet)
-        ans = []
-        for i in range(l):
-            tmp = [quiet[i]]
-            if i in bigger:
-                que = copy.deepcopy(bigger[i])
-                while que:
-                    # print(i,que,que[0], que[0] in bigger, bigger)
-                    tmp.append(quiet[que[0]])
-                    if que[0] in bigger:
-                        que += bigger[que[0]]
-                    del que[0]
-            # print(tmp)
+#         l = len(quiet)
+#         ans = []
+#         for i in range(l):
+#             tmp = [quiet[i]]
+#             if i in bigger:
+#                 que = copy.deepcopy(bigger[i])
+#                 while que:
+#                     # print(i,que,que[0], que[0] in bigger, bigger)
+#                     tmp.append(quiet[que[0]])
+#                     if que[0] in bigger:
+#                         que += bigger[que[0]]
+#                     del que[0]
+#             # print(tmp)
             
-            ans.append(quiet.index(min(tmp)))
-        # print(ans)
-        return ans
+#             ans.append(quiet.index(min(tmp)))
+#         # print(ans)
+#         return ans
 
 from collections import defaultdict
-
-
 class Solution:
     def loudAndRich(self, richer, quiet):
         graph = defaultdict(list)
@@ -47,7 +45,7 @@ class Solution:
             richest.add(rich)
             if rich_man in richest:
                 richest.remove(rich_man)
-        
+
         print(graph, richest)
 
         ans = list(range(len(quiet)))
