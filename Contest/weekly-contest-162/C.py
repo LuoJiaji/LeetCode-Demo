03a27f13be2aff9d4481110e7d5ab46c35e8f248
Luojiaji <lt920@126.com>
2019-11-12 10:20:27 +0800

    add new method in weekly 162-C

--- Contest/weekly-contest-162/C.py
+++ Contest/weekly-contest-162/C.py
@@ -38,9 +38,46 @@ class Solution(object):
                     self.flag = True
         return count
 
-
+class Solution(object):
+    def closedIsland(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        if not grid:
+            return 0
         
+        def dfs(grid, row, col, x, y):
+            flag = True
+            
+            if grid[x][y] == 1:
+                return flag
+            else:
+                if x == 0 or x == row - 1 or y == 0 or y == col - 1:
+                    flag = False
 
+            grid[x][y] = 1
+
+            if x != 0:
+                flag = flag & dfs(grid, row, col, x - 1, y)
+            if x != row - 1:
+                flag = flag & dfs(grid, row, col, x + 1, y)
+            if y != 0:
+                flag = flag & dfs(grid, row, col, x, y - 1)
+            if y != col - 1:
+                flag = flag & dfs(grid, row, col, x, y + 1)
+            return flag 
+
+        row = len(grid)
+        col = len(grid[0])
+        count = 0
+        for i in range(row):
+            for j in range(col):
+                if grid[i][j] == 0:
+                    flag = dfs(grid, row, col, i, j)
+                    if flag:
+                        count += 1
+        return count        
 
 grid = [[1,1,1,1,1,1,1,0],[1,0,0,0,0,1,1,0],[1,0,1,0,1,1,1,0],[1,0,0,0,0,1,0,1],[1,1,1,1,1,1,1,0]]
 res = Solution().closedIsland(grid)
