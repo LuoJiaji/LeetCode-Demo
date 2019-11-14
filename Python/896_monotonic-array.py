81dacb1c426a2d2489d08a1a878c9bc47907e915
Luojiaji <lt920@126.com>
2019-11-14 19:49:47 +0800

    add 896

---
+++ Python/896_monotonic-array.py
@@ -0,0 +1,33 @@
+class Solution(object):
+    def isMonotonic(self, A):
+        """
+        :type A: List[int]
+        :rtype: bool
+        """
+        flag = A[-1] - A[0]
+
+        if flag >= 0:
+            for i in range(1, len(A)):
+                if A[i] - A[i - 1] < 0:
+                    return False
+        else:
+             for i in range(1, len(A)):
+                if A[i] - A[i - 1] > 0:
+                    return False
+        return True
+
+data = [1,2,2,3]
+res = Solution().isMonotonic(data)
+print(res)
+
+data = [6,5,4,4]
+res = Solution().isMonotonic(data)
+print(res)
+
+data = [1,3,2]
+res = Solution().isMonotonic(data)
+print(res)
+
+data = [1,1,1]
+res = Solution().isMonotonic(data)
+print(res)
