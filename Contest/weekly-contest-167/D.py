a636ce9967ee5694a7224ec40884ffd1dde37aa7
Luojiaji <lt920@126.com>
2019-12-15 20:36:54 +0800

    fix bug

--- Contest/weekly-contest-167/D.py
+++ Contest/weekly-contest-167/D.py
@@ -5,61 +5,67 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        q = [[0,0]]
-        state = [k]
+        q = {(0,0):k}
         ans = 0
         n = len(grid)
         m = len(grid[0])
+        print(m,n)
+        for i in grid:
+            print(i)
         if n == 1 and m == 1 and k <= 1:
-            return 1
+            return 0
         while q:
             l = len(q)
             ans += 1
-            curr_q = []
-            curr_s = []
+            curr_q = {}
             print(q)
-            for i,[x,y] in enumerate(q):
-                print(x,y)
-                s = state[i]
+            for x,y in q:
+                # print(x,y)
+                s = q[(x,y)]
                 if x+1 < m:
                     if grid[y][x+1] == 1 and s >= 1:
-                        curr_q.append([x+1,y])
-                        curr_s.append(s-1)
+                        if (x+1,y) in curr_q:
+                            curr_q[(x+1,y)] = max(curr_q[(x+1,y)], s-1)
+                        else:
+                            curr_q[(x+1,y)] =s-1
                         if [x+1, y] == [m-1, n-1]:
                             return ans
                     if grid[y][x+1] == 0:
-                        curr_q.append([x+1,y])
-                        curr_s.append(s)
+                        if (x+1,y) in curr_q:
+                            curr_q[(x+1,y)] = max(curr_q[(x+1,y)], s)
+                        else:
+                            curr_q[(x+1,y)] = s
                         if [x+1, y] == [m-1, n-1]:
                             return ans
-                if x-1 > 0:
-                    if grid[y][x-1] == 0:
-                        curr_q.append([x-1,y])
-                        curr_s.append(s)
-
+                if x-1 >= 0 and grid[y][x-1] == 0:
+                        if (x-1,y) in curr_q:
+                            curr_q[(x-1,y)] = max(curr_q[(x-1,y)], s)
+                        else:
+                            curr_q[(x-1,y)] = s
 
                 if y+1 < n:
                     if grid[y+1][x] == 1 and s >= 1:
-                        curr_q.append([x, y+1])
-                        curr_s.append(s-1)
+                        if (x, y+1) in curr_q:
+                            curr_q[(x, y+1)] = max(curr_q[(x, y+1)], s-1)
+                        else:
+                            curr_q[(x, y+1)] = s-1
                         if [x, y+1] == [m-1, n-1]:
                             return ans
                     if grid[y+1][x] == 0:
-                        curr_q.append([x, y+1])
-                        curr_s.append(s)
+                        if (x, y+1) in curr_q:
+                            curr_q[(x, y+1)] = max(curr_q[(x, y+1)], s)
+                        else:
+                            curr_q[(x, y+1)] = s
+
                         if [x, y+1] == [m-1, n-1]:
                             return ans
-                if y-1 > 0:
-
-                    if grid[y-1][x] == 0:
-                        curr_q.append([x, y-1])
-                        curr_s.append(s)
-
+                if y-1 >= 0 and grid[y-1][x] == 0:
+                        if (x, y-1) in curr_q:
+                            curr_q[(x, y-1)] = max(curr_q[(x, y-1)], s)
+                        else:
+                            curr_q[(x, y-1)] = s
             q = curr_q
-            state = curr_s
-
         return -1
-                         
 
 # grid = [[0,0,0],
 #         [1,1,0],
@@ -72,13 +78,18 @@ class Solution(object):
 
 
 # grid = [[0,1,1],
-#  [1,1,1],
-#  [1,0,0]]
+#         [1,1,1],
+#         [1,0,0]]
 # k = 1
 # res = Solution().shortestPath(grid, k)
 # print(res)
 
-grid = [[0,1,0,0,0,1,0,0],[0,1,0,1,0,1,0,1],[0,0,0,1,0,0,1,0]]
-k = 1
+# grid = [[0,1,0,0,0,1,0,0],[0,1,0,1,0,1,0,1],[0,0,0,1,0,0,1,0]]
+# k = 1
+# res = Solution().shortestPath(grid, k)
+# print(res)
+
+grid = [[0,0,1,0,1,1,1,0],[1,1,0,1,0,1,0,0],[1,1,0,0,1,0,1,1],[1,1,0,1,0,1,0,0],[1,0,0,1,0,1,0,1],[0,0,1,1,1,0,0,1],[0,1,0,1,1,1,1,0],[1,0,0,0,1,1,1,0],[0,0,0,1,0,0,0,1],[1,0,1,0,0,0,1,0],[1,0,1,0,1,1,1,1],[1,1,1,0,0,0,0,1],[0,0,1,1,1,1,0,0],[0,1,0,1,0,1,0,1],[1,1,0,0,1,0,0,0],[0,1,1,1,0,0,1,1],[0,1,0,0,0,0,1,0],[1,1,1,0,1,0,0,0],[1,0,1,1,1,1,1,0],[0,1,0,1,0,0,1,0],[1,1,1,1,0,1,0,1],[0,0,0,0,0,0,1,1],[0,1,0,0,0,0,1,1],[1,1,1,0,0,0,1,1],[0,1,0,0,1,0,0,1],[1,0,0,1,0,1,0,0],[0,0,0,0,1,1,0,1],[0,0,1,0,1,0,1,0],[0,1,1,1,1,0,1,0]]
+k = 3
 res = Solution().shortestPath(grid, k)
 print(res)
